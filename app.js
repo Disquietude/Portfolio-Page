a38ecacd9e58722be8a491008d2b86965fde5400
Disquietude <danny93ngo@gmail.com>
2018-08-19 00:45:38 -0400

    Added responsive navbar

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ $(document).ready(function () {
     $("body, html").animate({
       scrollTop: $(this.hash).offset().top - 50
     }, 1000);
+
+    //On small screens, hide the menu after a link is clicked
+    if($(".responsive-menu").css("display") !== "none") {
+      $("#navbar .nav-link").hide();
+    }
   });
 
   //Scrollspy effect
@@ -31,5 +36,25 @@ $(document).ready(function () {
     navlink.eq(n-1).siblings().removeClass("active");
   });
 
+  //Responsive navbar
+  $(".fa-bars").click(function() { 
+    $("#navbar .nav-link").toggle();
+  });
 
+   //Show navbar if window is resized to > 1024px and hide it if < 1024px
+  $(window).resize(function() {
+    if($(".responsive-menu").css("display") == "none") {
+      $("#navbar .nav-link").show();
+    }
+    else {
+      $("#navbar .nav-link").hide();
+    }
+  });
+
+   //Hide responsive menu if user clicks anywhere outside the menu 
+  $("#navbar").siblings().click(function() { 
+    if($(".responsive-menu").css("display") !== "none") {
+      $("#navbar .nav-link").hide();
+    }
+  });
 });
